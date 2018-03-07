5c3a8655ab9884dd43b4ba595ffa8b52fcd5b2cb
Allan Egidio <allan.egidio@outlook.com>
2018-03-07 20:05:59 -0300

    Create Gig Add Feature

---
+++ GigHub.MVC/Controllers/GigsController.cs
@@ -0,0 +1,30 @@
+﻿using GigHub.MVC.Models;
+using GigHub.MVC.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.MVC.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            var gigViewModel = new GigFormViewModel();
+            gigViewModel.Genres = _context.Genres.ToList();
+
+            return View(gigViewModel);
+        }
+    }
+}
---
+++ GigHub.MVC/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GigHub.MVC.Models;
+using System.Collections.Generic;
+
+namespace GigHub.MVC.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
