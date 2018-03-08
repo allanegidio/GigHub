f099b28d8293e83194e547915d5b100e6417082e
Allan Egidio <allan.egidio@outlook.com>
2018-03-07 23:02:41 -0300

    Refactor create a gig

--- GigHub.MVC/Controllers/GigsController.cs
+++ GigHub.MVC/Controllers/GigsController.cs
@@ -1,5 +1,6 @@
 ﻿using GigHub.MVC.Models;
 using GigHub.MVC.ViewModels;
+using Microsoft.AspNet.Identity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -19,12 +20,33 @@ namespace GigHub.MVC.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
-            var gigViewModel = new GigFormViewModel();
-            gigViewModel.Genres = _context.Genres.ToList();
+            var gigViewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
 
             return View(gigViewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = viewModel.DateTime,
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- GigHub.MVC/ViewModels/GigFormViewModel.cs
+++ GigHub.MVC/ViewModels/GigFormViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GigHub.MVC.Models;
+using System;
 using System.Collections.Generic;
 
 namespace GigHub.MVC.ViewModels
@@ -8,7 +9,8 @@ namespace GigHub.MVC.ViewModels
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
-        public int Genre { get; set; }
+        public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+        public DateTime DateTime => DateTime.Parse($"{Date} {Time}");
     }
 }
