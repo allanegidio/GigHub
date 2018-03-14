4b4e4bfbfce97ac214059e933500302802f130e0
Allan Egidio <allan.egidio@outlook.com>
2018-03-14 19:12:35 -0300

    improve design on index home controller

--- GigHub.MVC/Controllers/HomeController.cs
+++ GigHub.MVC/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace GigHub.MVC.Controllers
         {
             var upcomingGigs = _context.Gigs
                                        .Include(x => x.Artist)
+                                       .Include(x => x.Genre)
                                        .Where(x => x.DateTime > DateTime.Now);
             return View(upcomingGigs);
         }
